8097ea1c362bf9cd1a9c145324d135fa7f892b73
CrisRonda <cristian.j005@gmail.com>
2020-12-31 16:12:29 -0500

    fix redux

--- App.js
+++ App.js
@@ -2,14 +2,20 @@ import React from 'react';
 import {StatusBar} from 'react-native';
 import Notifications from './src/wrappers/Notifications';
 import Navigation from './src/navigation';
+import {Provider} from 'react-redux';
+import {store, persistor} from './src/redux/';
+import {PersistGate} from 'redux-persist/integration/react';
+
 const App = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <Notifications>
-        <Navigation />
-      </Notifications>
-    </>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <StatusBar barStyle="dark-content" animated translucent />
+        <Notifications>
+          <Navigation />
+        </Notifications>
+      </PersistGate>
+    </Provider>
   );
 };
 
